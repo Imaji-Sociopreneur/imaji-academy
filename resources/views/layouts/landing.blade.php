44ebde39ffc2b6f523e77b6d7b5065ccc64567a2
Haidar <haidarcorp22@gmail.com>
2020-12-01 14:11:20 +0700

    Penyesuaian front end

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -50,7 +50,7 @@
                                 <a class="nav-link" href="{{ route('about') }}">About</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="cource.html">Courses</a>
+                                <a class="nav-link" href="cource.html">Berita</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="blog.html">Blog</a>
